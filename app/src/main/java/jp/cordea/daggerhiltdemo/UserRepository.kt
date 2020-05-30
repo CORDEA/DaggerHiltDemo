850179dbf08e61dfd9d49d811756e59e1e5542ea
Yoshihiro Tanaka <contact@cordea.jp>
2020-05-30 15:03:28 +0900

    implement repository

--- app/src/main/java/jp/cordea/daggerhiltdemo/UserRepository.kt
+++ app/src/main/java/jp/cordea/daggerhiltdemo/UserRepository.kt
@@ -1,5 +1,8 @@
 package jp.cordea.daggerhiltdemo
 
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.emitAll
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -8,4 +11,12 @@ class UserRepository @Inject constructor(
     private val remoteDataSource: UserRemoteDataSource,
     private val localDataSource: UserLocalDataSource
 ) {
+    @ExperimentalCoroutinesApi
+    fun findAll(forceRefresh: Boolean) =
+        if (forceRefresh) {
+            remoteDataSource.findAll()
+        } else {
+            localDataSource.findAll()
+                .catch { emitAll(remoteDataSource.findAll()) }
+        }
 }
