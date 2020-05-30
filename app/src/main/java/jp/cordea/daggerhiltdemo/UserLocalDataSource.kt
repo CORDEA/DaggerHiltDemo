a6ee59b449530d2356d9b339949c13c79e05382c
Yoshihiro Tanaka <contact@cordea.jp>
2020-05-30 15:03:09 +0900

    implement data sources

--- app/src/main/java/jp/cordea/daggerhiltdemo/UserLocalDataSource.kt
+++ app/src/main/java/jp/cordea/daggerhiltdemo/UserLocalDataSource.kt
@@ -1,8 +1,15 @@
 package jp.cordea.daggerhiltdemo
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class UserLocalDataSource @Inject constructor() {
+    private val cache: List<User>? = null
+
+    fun findAll(): Flow<List<User>> = flow {
+        emit(cache ?: error("Cache is unavailable"))
+    }
 }
--- app/src/main/java/jp/cordea/daggerhiltdemo/UserRemoteDataSource.kt
+++ app/src/main/java/jp/cordea/daggerhiltdemo/UserRemoteDataSource.kt
@@ -1,8 +1,26 @@
 package jp.cordea.daggerhiltdemo
 
+import io.bloco.faker.Faker
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class UserRemoteDataSource @Inject constructor() {
+    private val faker = Faker()
+
+    @ExperimentalCoroutinesApi
+    fun findAll(): Flow<List<User>> = flowOf(
+        (0 until 30).map {
+            User(
+                faker.name.firstName(),
+                faker.name.lastName(),
+                faker.phoneNumber.phoneNumber(),
+                faker.internet.safeEmail(),
+                faker.company.name()
+            )
+        }
+    )
 }
