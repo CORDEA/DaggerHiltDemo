44dc04e8c6b73a9edf21dd9b684f88544d4067ec
Yoshihiro Tanaka <contact@cordea.jp>
2020-05-30 15:01:24 +0900

    define user

---
+++ app/src/main/java/jp/cordea/daggerhiltdemo/User.kt
@@ -0,0 +1,9 @@
+package jp.cordea.daggerhiltdemo
+
+class User(
+    val firstName: String,
+    val lastName: String,
+    val phoneNumber: String,
+    val emailAddress: String,
+    val company: String
+)
