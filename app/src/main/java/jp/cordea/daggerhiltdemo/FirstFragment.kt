22d5be96a333a60f2af2735943c12bc18c5c189a
Yoshihiro Tanaka <contact@cordea.jp>
2020-05-30 16:28:56 +0900

    display items

--- app/src/main/java/jp/cordea/daggerhiltdemo/FirstFragment.kt
+++ app/src/main/java/jp/cordea/daggerhiltdemo/FirstFragment.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
 import dagger.hilt.android.AndroidEntryPoint
+import jp.cordea.daggerhiltdemo.databinding.FragmentFirstBinding
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import javax.inject.Inject
 
 @AndroidEntryPoint
+@ExperimentalCoroutinesApi
 class FirstFragment : Fragment() {
     @Inject
     lateinit var factory: FirstViewModel.Factory
@@ -22,13 +26,13 @@ class FirstFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View = inflater.inflate(R.layout.fragment_first, container, false)
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+    ): View {
+        val binding = FragmentFirstBinding.inflate(inflater, container, false)
+        val adapter = GroupAdapter<GroupieViewHolder>()
+        binding.recyclerView.adapter = adapter
+        viewModel.items.observe(viewLifecycleOwner, Observer { items ->
+            adapter.update(items.map { UserListItem(UserListItemViewModel.from(it)) })
+        })
+        return binding.root
     }
 }
