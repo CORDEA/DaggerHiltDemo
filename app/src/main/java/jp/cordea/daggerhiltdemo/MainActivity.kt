005d54df09fc991424f619cdca2d22d42fb8a1e1
Yoshihiro Tanaka <contact@cordea.jp>
2020-05-31 15:10:48 +0900

    remove menu

--- app/src/main/java/jp/cordea/daggerhiltdemo/MainActivity.kt
+++ app/src/main/java/jp/cordea/daggerhiltdemo/MainActivity.kt
@@ -1,8 +1,6 @@
 package jp.cordea.daggerhiltdemo
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,15 +10,4 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean =
-        when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
 }
